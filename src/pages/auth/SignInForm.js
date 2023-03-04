0f6952f8a64bc4e1897b1560c4b22c0914955b2b
AASanusi <alaminm.sanusi@gmail.com>
2023-03-04 11:46:41 +0000

    Tidy up code and final deployment

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -41,7 +41,7 @@ function SignInForm() {
       setTokenTimestamp(data);
       history.goBack();
     } catch (err) {
-      console.log(err)
+      // console.log(err)
       setErrors(err.response?.data);
     }
   };
